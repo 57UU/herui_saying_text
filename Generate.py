45f0aa2e6abc17d8894c8e5f574e75381fed6e2d
QY W <uu57uu@yandex.com>
2024-08-02 21:06:22 +0800

    update

--- Generate.py
+++ Generate.py
@@ -3,7 +3,7 @@ import os
 f=open("sayings.txt",encoding="utf-8")
 result=[]
 for i in f.readlines():
-    if(i[0]=="#" or i.strip()==""):
+    if(i[0]=="#" or i.strip()=="" or i.strip()=="\n"):
         continue
     result.append(i)
 f.close()
